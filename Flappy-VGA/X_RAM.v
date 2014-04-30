674aeab295d7ac649bd1102b61211a9237678329
lorsposto <llsposto@usc.edu>
2014-04-30 16:25:21 -0700

    x_ram

--- Flappy-VGA/X_RAM.v
+++ Flappy-VGA/X_RAM.v
@@ -23,31 +23,36 @@
 // I guess the clock would be at whatever rate ... not sure actually;
 // Coordinate is the left edge of the pipe. Right edge is calculated in obstacle logic.
 //////////////////////////////////////////////////////////////////////////////////
-module X_RAM_NOREAD(clk,reset,Start, Stop, Ack, Output, out_pipe, Score,
-	X_Edge_O1,
-	X_Edge_O2,
-	X_Edge_O3,
+module X_RAM_NOREAD(clk,reset,Start, Stop, Ack, out_pipe, Score,
+	X_Edge_OO_L,
+	X_Edge_O1_L,
+	X_Edge_O2_L,
+	X_Edge_O3_L,
+	X_Edge_OO_R,
+	X_Edge_O1_R,
+	X_Edge_O2_R,
+	X_Edge_O3_R,
 	Q_Initial,
 	Q_Count,
 	Q_Stop);
 
-	parameter X0_init = 0;
-	parameter X1_init = 160;
-	parameter X2_init = 320;
-	parameter X3_init = 480;
-
  input clk,reset;
  input Start, Stop, Ack;
  
- output [9:0] Output;
- output [9:0] X_Edge_O1;
- output [9:0] X_Edge_O2;
- output [9:0] X_Edge_O3;
+ output [9:0] X_Edge_OO_L;
+ output [9:0] X_Edge_O1_L;
+ output [9:0] X_Edge_O2_L;
+ output [9:0] X_Edge_O3_L;
+ output [9:0] X_Edge_OO_R;
+ output [9:0] X_Edge_O1_R;
+ output [9:0] X_Edge_O2_R;
+ output [9:0] X_Edge_O3_R;
  output	[1:0] out_pipe;
  output	[3:0] Score;
  output 	Q_Initial, Q_Count, Q_Stop;
  
- reg [9:0] array_X [3:0]; // 2D array X to store four 10 bit pipes
+ reg [9:0] array_X_Left [3:0]; // 2D array X to store four 10 bit pipes
+ reg [9:0] array_X_Right [3:0]; // 2D array X to store four 10 bit pipes
  reg [1:0] out_pipe;
  reg [3:0] Score;
  
@@ -61,8 +66,24 @@ module X_RAM_NOREAD(clk,reset,Start, Stop, Ack, Output, out_pipe, Score,
 	
 	assign {Q_Stop, Q_Count,
 			Q_Initial } = state;
+			
+	parameter X0_init = 0;
+	parameter X1_init = 160;
+	parameter X2_init = 320;
+	parameter X3_init = 480;
+	
+	parameter X0_init_2 = 80;
+	parameter X1_init_2 = 240;
+	parameter X2_init_2 = 400;
+	parameter X3_init_2 = 560;
+	
+	//reg [1:0] out_temp_0;
+	reg [1:0] out_temp_1;
+	reg [1:0] out_temp_2;
+	reg [1:0] out_temp_3;
 
-//integer i;
+
+integer i;
  always @ (posedge clk, posedge reset)
 	begin  : X_RAM_logic
 		if(reset)
@@ -80,11 +101,19 @@ module X_RAM_NOREAD(clk,reset,Start, Stop, Ack, Output, out_pipe, Score,
 						state <= QCount;
 					
 						Score <= 0;
-						array_X[0] <= X0_init;
-						array_X[1] <= X1_init;
-						array_X[2] <= X2_init;
-						array_X[3] <= X3_init;
+						array_X_Left[0] <= X0_init;
+						array_X_Left[1] <= X1_init;
+						array_X_Left[2] <= X2_init;
+						array_X_Left[3] <= X3_init;
+						
+						array_X_Right[0] <= X0_init_2;
+						array_X_Right[1] <= X1_init_2;
+						array_X_Right[2] <= X2_init_2;
+						array_X_Right[3] <= X3_init_2;
 						out_pipe <= 2; // The first pipe in scope is 2 because that's just to the right of the bird.
+						out_temp_1 <= 3;
+						out_temp_2 <= 0;
+						out_temp_3 <= 1;
 				end	
 			
 				QCount:
@@ -93,21 +122,35 @@ module X_RAM_NOREAD(clk,reset,Start, Stop, Ack, Output, out_pipe, Score,
 					begin
 						state <= QStop;
 					end
-						
-						//for(i = 0; i < 4; i = i + 1) // Then we shift each pipe by 1 pixel
-						//begin
-							array_X[0] <= array_X[0]-10'd1;
-							array_X[1] <= array_X[1]-10'd1;
-							array_X[2] <= array_X[2]-10'd1;
-							array_X[3] <= array_X[3]-10'd1;
-						//end
+						for(i = 0; i < 4; i = i + 1) // Then we shift each pipe by 1 pixel
+						begin
+							array_X_Left[i] <= array_X_Left[i]-10'd1;
+							if(array_X_Left[i] == 0)
+								array_X_Left[i] <= 640;
+								
+							array_X_Right[i] <= array_X_Right[i]-10'd1;
+							if(array_X_Right[i] == 0)
+								array_X_Right[i] <= 640;
+						end
 				
-						if(array_X[out_pipe] < 240) // if current pipe is going out of scope (240 because 320-width of 80)
+						if(array_X_Right[out_pipe] < 320) // if current pipe is going out of scope (240 because 320-width of 80)
 						begin // move on to the next pipe
 							out_pipe <= out_pipe + 1;
 							if(out_pipe == 3)
 								out_pipe <= 0;
-						
+							
+							out_temp_1 <= out_temp_1 + 1;
+							if(out_temp_1 == 3)
+								out_temp_1 <= 0;
+								
+							out_temp_2 <= out_temp_2 + 1;
+							if(out_temp_2 == 3)
+								out_temp_1 <= 0;
+								
+							out_temp_3 <= out_temp_3 + 1;
+							if(out_temp_3 == 3)
+								out_temp_3 <= 0;
+							
 							Score <= Score + 4'd1; // increment score once a pipe passes the bird
 						end
 				
@@ -126,9 +169,14 @@ module X_RAM_NOREAD(clk,reset,Start, Stop, Ack, Output, out_pipe, Score,
 	end
 
  
- assign Output = array_X[out_pipe];
- assign X_Edge_O1 = array_X[out_pipe-1];
- assign X_Edge_O2 = array_X[out_pipe-2];
- assign X_Edge_O3 = array_X[out_pipe-3];
+ assign X_Edge_OO_L = array_X_Left[out_pipe];
+ assign X_Edge_O1_L = array_X_Left[out_temp_1];
+ assign X_Edge_O2_L = array_X_Left[out_temp_2];
+ assign X_Edge_O3_L = array_X_Left[out_temp_3];
+ 
+ assign X_Edge_OO_R = array_X_Right[out_pipe];
+ assign X_Edge_O1_R = array_X_Right[out_temp_1];
+ assign X_Edge_O2_R = array_X_Right[out_temp_2];
+ assign X_Edge_O3_R = array_X_Right[out_temp_3];
  
 endmodule
--- Flappy-VGA/X_RAM_tb.v
+++ Flappy-VGA/X_RAM_tb.v
@@ -27,28 +27,53 @@ module X_RAM_tb;
 	// Inputs
 	reg clk;
 	reg reset;
-	reg count_EN;
+	reg Start;
+	reg Ack;
 
 	// Outputs
-	wire [9:0] Output;
 	wire [1:0] out_pipe;
 	wire [3:0] Score;
+	
+	wire [9:0] X_Edge_OO_L;
+	wire [9:0] X_Edge_O1_L;
+	wire [9:0] X_Edge_O2_L;
+	wire [9:0] X_Edge_O3_L;
+	
+	wire [9:0] X_Edge_OO_R;
+	wire [9:0] X_Edge_O1_R;
+	wire [9:0] X_Edge_O2_R;
+	wire [9:0] X_Edge_O3_R;
+	wire 	Q_Initial, Q_Count, Q_Stop;
 
 	// Instantiate the Unit Under Test (UUT)
 	X_RAM_NOREAD uut (
 		.clk(clk), 
 		.reset(reset), 
-		.count_EN(count_EN), 
-		.Output(Output), 
+		.Start(Start),
+		.Ack(Ack),
 		.out_pipe(out_pipe), 
-		.Score(Score)
+		.Score(Score),
+		.X_Edge_OO_L(X_Edge_OO_L),
+		.X_Edge_O1_L(X_Edge_O1_L),
+		.X_Edge_O2_L(X_Edge_O2_L),
+		.X_Edge_O3_L(X_Edge_O3_L),
+		
+		.X_Edge_OO_R(X_Edge_OO_R),
+		.X_Edge_O1_R(X_Edge_O1_R),
+		.X_Edge_O2_R(X_Edge_O2_R),
+		.X_Edge_O3_R(X_Edge_O3_R),
+		
+		.Q_Initial,
+		.Q_Count,
+		.Q_Stop
 	);
 
 	initial begin
 		// Initialize Inputs
 		clk = 0;
 		reset = 0;
-		count_EN = 0;
+		Start = 0;
+		Ack = 0;
 
 		// Wait 100 ns for global reset to finish
 		#100;
@@ -56,11 +81,18 @@ module X_RAM_tb;
 		#20;
 		reset = 0;
 		#20;
-      count_EN = 1;
+		Start = 1;
+		#50;
+		Start = 0;
+		#500;
+		Ack = 1;
+		#50;
+		Ack = 0;
+		#50;
 		
 		// Add stimulus here
 		
 	end
-      always begin #10; clk = ~ clk; end
+      always begin #20; clk = ~ clk; end
 endmodule
 
